945b593cd20df13bd364aac868b1ee4d40f711f4
shinyaoguri <ogrium@gmail.com>
2021-02-23 21:20:50 +0900

    feat(main.dart): 新しいスクリーンの追加
    いいねの一覧を新しいスクリーンでリスト表示させる

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,40 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Startup Name Generator"),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+              (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              });
+          final divided = ListTile.divideTiles(
+            tiles: tiles,
+            context: context,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Saved Suggestions"),
+            ),
+            body: ListView(children: divided,),
+          );
+        },
+      ),
     );
   }
 

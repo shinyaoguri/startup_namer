ba755612d7493d4663b2b3b0fa60671d9f10fb97
shinyaoguri <ogrium@gmail.com>
2021-02-23 18:47:59 +0900

    feat(main.dart, pubspec.yaml):

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Wellcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Welcome to Flutter"),
         ),
-        body: const Center(
-          child: const Text("Hello World"),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
